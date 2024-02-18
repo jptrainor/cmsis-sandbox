5dbc8660111528573f26591a58b58d0fd6d8464a
jptrainor <jim.trainor@jptrainor.com>
2024-02-18 16:47:46 -0500

    fix typos

--- octave/signal.m
+++ octave/signal.m
@@ -4,21 +4,23 @@ function s = signal(N, k, addNoise = false)
 #
 # Signal is an N sample sine wave with optional added noise. Without
 # noise the amplitude of the sine way is 1.0. With noise the sine
-# wave amplitude is 0.75 and the noise is distributed over [-0.25,
+# wave amplitude is 0.75 and added noise is distributed over [-0.25,
 # 0.25]. Either way, the range of the signal is [-1, 1].
 #
 # The sine wave is created using the formula sin(pi/k * n) where k is
-# the nyquist frequency divisor and n is the sample index. If k=1
-# then the waveform is generated at the nyquist frequency. If k>1 then
-# the wave is generated at a fraction of the nyquist frequency. For
-# example, k=2 is half the nyquist frequency. To generate one full
+# the Nyquist frequency divisor and n is the sample index. If k=1
+# then the waveform is generated at the Nyquist frequency. If k>1 then
+# the wave is generated at a fraction of the Nyquist frequency. For
+# example, k=2 is half the Nyquist frequency. To generate one full
 # period of the sine wave over N samples use k=N/2.
 #
 # The value of k is not validated. If k=1 the resulting waveform won't
-# be a recognizable sine wave because a discreet sin wave cannot be
-# represented at the nyquist frequency. Values of k<1 produce aliased
-# waveforms. Non integer values of k produce a waveform with unevenly
+# be a recognizable sine wave because a discrete sin wave cannot be
+# represented at the Nyquist frequency. Values of k<1 produce aliased
+# waveforms. Non-integer values of k produce a waveform with unevenly
 # sample amplitude. This is particularly noticable when 1<k<2.
+#
+# This matches the test waveform by the cmsis-sandbox benchmarks.
  
   s = sin([0:1:(N-1)]*pi/k);
   if ( addNoise == true )
